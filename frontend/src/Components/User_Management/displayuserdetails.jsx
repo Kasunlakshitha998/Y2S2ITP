3bce859482a55a7c399a5fe14c2b16ccc0722401
navinda-viraj <158707439+navinda-viraj@users.noreply.github.com>
2024-04-04 17:36:42 +0530

    update

--- frontend/src/Components/User_Management/displayuserdetails.jsx
+++ frontend/src/Components/User_Management/displayuserdetails.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import "./displayuser.css"; // Import your CSS file
+import Swal from 'sweetalert2';
 
 function Users() {
     const [users, setUsers] = useState([]);
@@ -25,17 +26,37 @@ function Users() {
     });
 
     const handleDelete = (userId) => {
-        axios.delete(`http://localhost:8175/user/deleteUser/${userId}`)
-            .then((response) => {
-                console.log(response);
-                setUsers(users.filter((user) => user._id !== userId));
-                alert('User deleted successfully.');
-            })
-            .catch((error) => {
-                console.error('Error deleting user:', error);
-                alert('Failed to delete user. Please try again later.');
-            });
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Should you delete this?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete(`http://localhost:8175/user/deleteUser/${userId}`)
+                    .then(() => {
+                        setUsers(users.filter((user) => user._id !== userId));
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "User deleted successfully.",
+                            icon: "success"
+                        });
+                    })
+                    .catch((error) => {
+                        console.error('Error deleting user:', error);
+                        Swal.fire({
+                            title: "Error!",
+                            text: "Failed to delete user. Please try again later.",
+                            icon: "error"
+                        });
+                    });
+            }
+        });
     };
+    
 
     return (
         <div className="container-fluid">
