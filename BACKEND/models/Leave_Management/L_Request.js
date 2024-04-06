1cdd16f2f2fa00c7c30cf19e07917e903fe78a4e
K. Pramadu Shehan Chithakshana <151552571+Pramadu2001@users.noreply.github.com>
2024-04-07 00:24:15 +0530

    request form

---
+++ BACKEND/models/Leave_Management/L_Request.js
@@ -0,0 +1,93 @@
+const mongoose =require('mongoose');
+const Schema =mongoose.Schema;
+
+const L_RequestSchema = new schema({
+
+    requestId: {
+        type: String,
+        unique: true, 
+        required: true
+      },
+      
+    EmpID:{
+        type:String,
+        required: true
+    },
+   
+    name: {
+        type:String,
+        required: true
+
+    },
+
+    Email:{
+        type:String,
+        required : true
+    },
+    
+    Contact:{
+        type:Number,
+        required : true
+    },
+
+
+    Destination:{
+             type:String, 
+             required : true
+
+    },
+
+
+    LDateF:{
+        type:Date,
+        required : true
+
+    },
+
+    LdateT:{
+        type:Date,
+        required : true
+    },
+
+    LType:{
+        type:String,
+        required : true
+    },
+
+    Lduration:{
+        type:String,
+        required : true
+    },
+
+    attachments:{
+        type:Object,
+
+    },
+
+    remarks:{
+        type:String,
+
+    },
+
+    //supervisor 
+    Sup_name:{
+        type:String,
+
+    },
+
+    Sup_des:{
+        type:String,
+
+    },
+
+    //employee who cover his part
+    Backup:{
+        type:String,
+
+    },
+
+
+})
+
+const L_Request = mongoose.model("L_Request",L_RequestSchema);
+module.exports = L_Request;
