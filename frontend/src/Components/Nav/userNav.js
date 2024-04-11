20f09254865fdd57a4f8765b46f0f7ff76ce793d
waruna-weerasinghe <it22578846@my.sliit.lk>
2024-04-11 11:40:20 +0530

    add a appointment to the adda image functionality

--- frontend/src/Components/Nav/userNav.js
+++ frontend/src/Components/Nav/userNav.js
@@ -83,7 +83,7 @@ function UserNav() {
               </li>
               <li>
                 <Link
-                  to="#"
+                  to="/addForm"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                 >
                   Services
