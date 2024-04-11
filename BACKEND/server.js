b7e5ce8442e91d7da1e58b6427570c7b4e03346e
kasun lakshitha <128232048+IT22564436@users.noreply.github.com>
2024-04-12 00:29:43 +0530

    product page update

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -10,12 +10,15 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 8175;
 
 // Middleware
+app.use(express.json({ extended: false, limit: '50mb' }));
+app.use(
+  express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 })
+);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(bodyParser.json({ limit: '50mb' })); 
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 
 
 
