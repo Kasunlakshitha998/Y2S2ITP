2f41a6b1db0d607099b90d4d3daadfd84af32f58
DRK-SIDE <161360258+DRK-SIDE@users.noreply.github.com>
2024-04-16 18:01:10 +0530

    Revert "Merge branch '#Develop' of https://github.com/IT22564436/Y2S2ITP into #Develop"
    This reverts commit 551c4ed06b3171eb581431ccbe64ba5941515dbe, reversing
    changes made to e2068c366152d1d6bfb27ca5bec769e13699c111.

--- BACKEND/routes/feedback management/feedbacks.js
+++ BACKEND/routes/feedback management/feedbacks.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
-let Feedback = require('../../models/feedback management/feedback');
+//let Feedback = ("../models/Feedback");
+let Feedback = require("../models/Feedback");
 
 
+
+http://Localhost:8070/feedback/add
 
 router.route("/add").post((req,res)=>{
 
-    const productId = req.body.productId;
+    const feedbackId = req.body.feedbackId;
     const userId = req.body.userId;
     const date = req.body.date;
     const name = req.body.name;
@@ -15,14 +18,15 @@ router.route("/add").post((req,res)=>{
 
 
     const newFeedback = new Feedback({
-      productId,
-      userId,
-      date,
-      name,
-      email,
-      rating,
-      feedbackType,
-    });
+
+        feedbackId,
+        userId,
+        date,
+        name,
+        email,
+        rating,
+        feedbackType
+    })
 
     newFeedback.save().then(()=>{
         res.json("Feedback Added")
@@ -45,33 +49,28 @@ router.route("/").get((req,res)=>{
 
 })
 
-//http//Localhost:8175/feedback/update/
+http//Localhost:8070/feedback/update/
 
 router.route("/update/:id").put(async (req, res)=>{
 
     let feedbackId = req.params.id;
-    const { productId, userId, date, name, email, rating,feedbackType } = req.body;
+    const { name,email,rating,feedbackType } = req.body;
 
     const updateFeedback = {
-      productId,
-      userId,
-      date,
-      name,
-      email,
-      rating,
-      feedbackType,
-    };
+        name,
+        email,
+        rating,
+        feedbackType
+    }
 
-    const update = await Feedback.findByIdAndUpdate(feedbackId, updateFeedback)
-      .then((feedback) => {
-        res.status(200).send({ status: 'Feedback Updated', feedback });
-      })
-      .catch((err) => {
-        console.log(err);
-        res
-          .status(500)
-          .send({ status: 'Error with updating data', error: err.message });
-      });
+    const update = await Feedback.findByAndUpdate(feedbackId, updateFeedback)
+    .then(() => {
+
+    res.status(200).send({status: "Feedback Updated", feedback: update})
+    }).catch((err)=>{
+   console.log(err);
+   res.status(500).send({status: "Error with updating data",error:err.message}); 
+    })
 })
 
 router.route("/delete/:id").delete(async (req, res) => {
@@ -79,8 +78,8 @@ router.route("/delete/:id").delete(async (req, res) => {
 
     await Feedback.findByIdAndDelete(feedbackId)
     .then(() => {
-        res.status(200).send({ status: 'feedback deleted' });
-    }).catch((err) => {
+        res.status(200).send({status: "User deleted"});
+    }).catch((errr) => {
         console.log(err.message);
         res.status(500).send({status: "Error with delete feedback",error:err.message})
     })
@@ -89,15 +88,12 @@ router.route("/delete/:id").delete(async (req, res) => {
 router.route("/get/:id").get(async (req,res) => {
     let feedbackId = req.params.id;
     await Feedback.findById(feedbackId)
-      .then((feedback) => {
-        res.status(200).send({ status: 'Feedback fetched', feedback });
-      })
-      .catch((err) => {
+    .then(() => {
+        res.status(200).send({status: "Feedback fetched",feedback: feedback})
+    }).catch(() => {
         console.log(err.message);
-        res
-          .status(500)
-          .send({ status: 'Error with get user', error: err.message });
-      });
+        res.status(500).send({status: "Error with get user",error: err.message});
+    })
 })
 
 
