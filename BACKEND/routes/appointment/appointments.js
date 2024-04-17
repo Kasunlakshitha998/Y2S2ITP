e8cd25685bcfcac5c9785b9e4e986b882640eef2
waruna-weerasinghe <it22578846@my.sliit.lk>
2024-04-17 06:29:09 +0530

    edit appointment

--- BACKEND/routes/appointment/appointments.js
+++ BACKEND/routes/appointment/appointments.js
@@ -9,7 +9,7 @@ const Appointment = require("../../models/appointment/appointment"); // Adjusted
 // Create operation (Create a new appointment)
 router.route("/add").post((req, res) => {
     // Extract data from request body
-    const { name, email, telephone,phoneType,serviceType, date, description} = req.body;
+    const { name, email, telephone,phoneType,serviceType, date, description,image} = req.body;
 
     // Create a new appointment object
     const newAppointment = new Appointment({
@@ -20,7 +20,7 @@ router.route("/add").post((req, res) => {
         serviceType,
         date,
         description,
-        //image
+        image
 
     });
 
@@ -31,13 +31,13 @@ router.route("/add").post((req, res) => {
         console.log(err);
     })
 
-        // .then(() => {
-        //     res.json({ message: "Appointment added successfully" }); // Adjusted response message format
-        // })
-        // .catch((err) => {
-        //     console.log(err);
-        //     res.status(500).json({ error: err.message }); // Adjusted response status and error message
-        // });
+        .then(() => {
+            res.json({ message: "Appointment added successfully" }); // Adjusted response message format
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: err.message }); // Adjusted response status and error message
+        });
 });
 
 // Read operation (Get all appointments)
@@ -55,7 +55,7 @@ router.get("/", (req, res) => {
 // Update operation (Update an existing appointment)
 router.put("/update/:id", async (req, res) => {
     const userId = req.params.id;
-    const { name, email, telephone,phoneType,serviceType, date, description} = req.body;
+    const { name, email, telephone,phoneType,serviceType, date, description, image} = req.body;
 
     const updateAppointment = {
         name, 
@@ -64,8 +64,8 @@ router.put("/update/:id", async (req, res) => {
         phoneType,
         serviceType,
         date, 
-        description
-        //image
+        description,
+        image
     };
 
     // Update the appointment by ID
